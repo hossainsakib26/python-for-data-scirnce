9ee7cb4dc2a27ddc9597c4768b4f2670836812cb
Sazzad Hossain <hossain.sakib26@hotmail.com>
2023-10-27 13:54:16 +0100

    reading and saving file using numpy array complite

--- week3/lecture1_file_input_output.py
+++ week3/lecture1_file_input_output.py
@@ -1,13 +1,18 @@
+#reading and save file using numpy
 import numpy as np
 import random as rnd
 
-myAray = np.empty((3,2), dtype=int)
+myAray = np.empty((3, 4), dtype=int)
 print(myAray)
 
 for i in range(0, 3):
-    for j in range(0, 2):
+    for j in range(0, 4):
         myAray[i,j] = rnd.randint(1, 99)
-        print(myAray[i,j], end=',')
-    print("")
+        print(myAray[i,j], end='')
+    print('')
 
+#save my text
 np.savetxt('myArray.csv', myAray, fmt='%d', delimiter=',')
+#read my file
+reading_file = np.genfromtxt('MyArray.csv', delimiter=',', dtype='unicode')
+print(reading_file)
