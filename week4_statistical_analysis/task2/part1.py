4c717dbbcbf3eff151092d855889c73604d54ff5
Sazzad Hossain <hossain.sakib26@hotmail.com>
2023-11-17 14:30:58 +0000

    complete work in part 5 task 2 for week 4 histogram review needed

--- week4_statistical_analysis/task2/part1.py
+++ week4_statistical_analysis/task2/part1.py
@@ -26,3 +26,39 @@ SDByRow = np.std(MyDataSet2, axis=1)
 print('from line 23:', SDByRow) #shows array of Standard Deviation
 
 #------------------------------------part3--------------------------------------
+
+#Part 3 Initialise a one-dimensional array representing a normal distribution of 1000 data
+#points with mean value 17 and standard deviation 0.2.
+Mean = float(17.0)
+SD = float(0.2)
+#dataset size
+Size = int(1000)
+#generate nomal distribution with 1000 datapoint and mean 17 and SD 0.2
+MyDataSet3 = np.random.normal(Mean, SD, Size)
+print('from line 37',MyDataSet3)
+
+#------------------------------------part4--------------------------------------
+
+#Find the maximum and the minimum values of the dataset and calculate the range
+MinimumValue = round(np.min(MyDataSet3), 2)
+print('minimum value of dataset 3: ', MinimumValue)
+MaximumValue = round(np.max(MyDataSet3), 2)
+print('minimum value of dataset 3: ', MaximumValue)
+RangeValue = MaximumValue - MinimumValue
+print('Range between minimum and maximum in dataset 3: ', round(RangeValue, 2))
+
+#------------------------------------part5--------------------------------------
+
+#Visualise the dataset by using a histogram with 10 bins. Visualise the probability density function.
+
+#Where X represents the data points, μ is the mean value and σ is the standard deviation
+
+#plot the histogram
+
+A,X,C = plt.hist(MyDataSet3, bins=10, density=True)
+#calculate the probability density function
+f = (1/ (SD*np.sqrt(2*np.pi)))*np.exp(-((X-Mean)**2/(2*SD**2)))
+# Plot the probability density function
+plt.plot(X, f)
+# Visualise the diagrams
+plt.show()
